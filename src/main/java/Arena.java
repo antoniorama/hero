27f30e65e797dd88e28bb60b19057af2afc7272d
antoniorama <92859799+tonirama@users.noreply.github.com>
2022-10-05 17:54:30 +0100

    12. Walls

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -8,17 +8,21 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
 
     private int width;
     private int height;
     private Hero hero;
+    private List<Wall> walls;
 
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
         hero = new Hero(10, 10);
+        this.walls = createWalls();
     }
 
     public boolean canHeroMove(Position position) {
@@ -26,6 +30,11 @@ public class Arena {
         if (position.getX() > width - 1) return false;
         if (position.getY() < 0) return false;
         if (position.getY() > height - 1) return false;
+
+        for (Wall wall : walls) {
+            if (wall.getPosition().getX() == position.getX() && wall.getPosition().getY() == position.getY()) return false;
+        }
+
         return true;
     }
 
@@ -42,8 +51,25 @@ public class Arena {
     }
 
     public void draw(TextGraphics graphics) {
+        //Background
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         this.hero.draw(graphics);
+        //Walls
+        for (Wall wall : walls)
+            wall.draw(graphics);
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
     }
 }
